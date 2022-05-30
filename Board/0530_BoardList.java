1c3fc572e4f38f30e17d47ddf2ce44fa5070199e
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-30 09:28:14 +0900

    Create 0530_BoardList.java

---
+++ Board/0530_BoardList.java
@@ -0,0 +1,42 @@
+package kr.or.ddit.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListAll
+ */
+@WebServlet("/BoardList.do")
+public class BoardList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//전체리스트 출력
+		
+		
+		//servlet 객체 얻어오기
+		
+		
+		//servlet 메소드 호출하기 - 결과 값 받기
+		
+		
+		//결과 값을 request에 저장
+		
+		
+		//view페이지로 이동
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//페이지별 리스트 출력
+		
+	}
+
+}
