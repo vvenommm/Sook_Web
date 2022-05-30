db1c3e73f88686265612e38642cf9d2594181bc1
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-30 09:47:02 +0900

    Create 0530_BoardServiceImpl.java

---
+++ Board/0530_BoardServiceImpl.java
@@ -0,0 +1,36 @@
+package kr.or.ddit.board.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.or.ddit.board.dao.BoardDaoImpl;
+import kr.or.ddit.board.dao.IBoardDao;
+import kr.or.ddit.board.vo.BoardVO;
+
+public class BoardServiceImpl implements IBoardService {
+	
+	private IBoardDao dao;
+	private static IBoardService service;
+	
+	private BoardServiceImpl() {
+		dao = BoardDaoImpl.getInstance();
+	}
+	
+	public static IBoardService getInstance() {
+		if(service == null) service = new BoardServiceImpl();
+		return service;
+	}
+
+	@Override
+	public List<BoardVO> selectAll() {
+		List<BoardVO> list = null;
+		
+		try {
+			list = dao.selectAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+}
