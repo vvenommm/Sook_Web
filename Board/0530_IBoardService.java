dbadda7f2dcec3159fe9ee9f630033eb8f11f7e5
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-30 09:49:27 +0900

    Create 0530_IBoardService.java

---
+++ Board/0530_IBoardService.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.board.service;
+
+import java.util.List;
+
+import kr.or.ddit.board.vo.BoardVO;
+
+public interface IBoardService {
+
+	//리스트 출력
+	public List<BoardVO> selectAll();
+	
+	
+	//페이지별 리스트 출력
+	
+	
+	//수정
+	
+	
+	//삭제
+	
+	
+	//글쓰기
+	
+	
+	//조회수 증가
+	
+	
+	//댓글 저장
+	
+	
+	//댓글 수정
+	
+	
+	//댓글 삭제
+	
+	
+	//전체 글 갯수 구하기
+	
+	
+	
+}
