358a4071d0ccf78b04e8b119c73046272b7a31e7
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-31 09:25:05 +0900

    Update 0530_IBoardService.java

--- Board/0530_IBoardService.java
+++ Board/0530_IBoardService.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.board.service;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.or.ddit.board.vo.BoardVO;
 
@@ -9,12 +10,11 @@ public interface IBoardService {
 	//리스트 출력
 	public List<BoardVO> selectAll();
 	
-	
 	//페이지별 리스트 출력
+	public List<BoardVO> selectByPage(Map<String, Object> map); //값으로 정수, 문자열 다 올 수 있기 때문에 Object로
 	
-	
-	//수정
-	
+	//전체 글 갯수 구하기
+	public int searchCount(Map<String, Object> map);
 	
 	//삭제
 	
@@ -34,7 +34,7 @@ public interface IBoardService {
 	//댓글 삭제
 	
 	
-	//전체 글 갯수 구하기
+	//수정
 	
 	
 	
