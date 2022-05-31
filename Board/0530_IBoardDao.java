dfb701f25547cd6947e8a923f0f1f07604801cb1
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-31 09:25:42 +0900

    Update 0530_IBoardDao.java

--- Board/0530_IBoardDao.java
+++ Board/0530_IBoardDao.java
@@ -2,29 +2,42 @@ package kr.or.ddit.board.dao;
 
 import java.sql.SQLException;
 import java.util.List;
-
-import com.ibatis.sqlmap.client.SqlMapClient;
+import java.util.Map;
 
 import kr.or.ddit.board.vo.BoardVO;
-import kr.or.ddit.ibatis.config.SqlMapClientFactory;
 
-public class BoardDaoImpl implements IBoardDao {
-	
-	private SqlMapClient client;
-	private static BoardDaoImpl dao;
+public interface IBoardDao {
 
-	private BoardDaoImpl() {
-		client = SqlMapClientFactory.getSqlMapClient();
-	}
+	//리스트 출력
+	public List<BoardVO> selectAll() throws SQLException;
 	
-	public static BoardDaoImpl getInstance() {
-		if(dao == null) dao = new BoardDaoImpl();
-		return dao;
-	}
 	
-	@Override
-	public List<BoardVO> selectAll() throws SQLException {
-		return client.queryForList("board.selectAll");
-	}
-
+	//페이지별 리스트 출력
+	public List<BoardVO> selectByPage(Map<String, Object> map) throws SQLException; //값으로 정수, 문자열 다 올 수 있기 때문에 Object로
+	
+	//전체 글 갯수 구하기
+	public int searchCount(Map<String, Object> map) throws SQLException;
+	
+	
+	//수정
+	
+	
+	//삭제
+	
+	
+	//글쓰기
+	
+	
+	//조회수 증가
+	
+	
+	//댓글 저장
+	
+	
+	//댓글 수정
+	
+	
+	//댓글 삭제
+	
+	
 }
