1e88209c0b6ce68399f81837ac18194670445e0f
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-03 12:36:30 +0900

    Update 0530_IBoardDao.java

--- Board/0530_IBoardDao.java
+++ Board/0530_IBoardDao.java
@@ -11,25 +11,23 @@ public interface IBoardDao {
 	//리스트 출력
 	public List<BoardVO> selectAll() throws SQLException;
 	
-	
 	//페이지별 리스트 출력
 	public List<BoardVO> selectByPage(Map<String, Object> map) throws SQLException; //값으로 정수, 문자열 다 올 수 있기 때문에 Object로
 	
 	//전체 글 갯수 구하기
 	public int searchCount(Map<String, Object> map) throws SQLException;
 	
-	
-	//수정
-	
+	//글쓰기
+	public int posting(BoardVO vo) throws SQLException;
 	
 	//삭제
+	public int deletePost(int num) throws SQLException;
 	
-	
-	//글쓰기
-	
+	//수정
+	public int editPost(BoardVO vo) throws SQLException;
 	
 	//조회수 증가
-	
+	public int updateHit(int num) throws SQLException;
 	
 	//댓글 저장
 	
