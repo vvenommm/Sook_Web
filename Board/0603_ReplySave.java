13f5d88c57d49eca7218466164e57c62e045a9a2
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-03 12:56:57 +0900

    Create 0603_ReplySave.java

---
+++ Board/0603_ReplySave.java
@@ -0,0 +1,44 @@
+package kr.or.ddit.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.ddit.board.service.BoardServiceImpl;
+import kr.or.ddit.board.service.IBoardService;
+import kr.or.ddit.board.vo.ReplyVO;
+
+@WebServlet("/ReplySave.do")
+public class ReplySave extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		ReplyVO vo = null;
+		//데이터 받아오기
+		vo.setBonum((int)request.getAttribute("bonum"));
+		vo.setName((String)request.getAttribute("name"));
+		vo.setCont((String)request.getAttribute("cont"));
+		
+		// service 객체 얻어오기
+		IBoardService service = BoardServiceImpl.getInstance();
+
+		// service 메소드 호출하기 -> 결과값 받기
+		int resultNum = service.replySave(vo);
+
+		// request에 결과값 저장하기
+		request.setAttribute("resultNum", resultNum);
+
+		// view 페이지로 이동
+		request.getRequestDispatcher("view/resultNum.jsp").forward(request, response);
+		
+		
+		
+	}
+
+}
